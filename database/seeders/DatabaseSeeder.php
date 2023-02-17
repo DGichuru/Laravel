5c8bbcf78928296ba082273241984e9a227e44e0
DGichuru <dangichuru77@gmail.com>
2023-02-17 12:43:21 +0300

    listings factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Listing;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,11 +15,34 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(5)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
+        Listing::factory(6)->create();
+
+        // Listing::create([
+        //     'title' => 'Laravel Senior Developer',
+        //     'tags' => 'laravel javascript',
+        //     'company' => 'Acme Corp',
+        //     'location' => 'Boston, MA',
+        //     'email' => 'email1@mail.com',
+        //     'website' => 'https:/www.asne.com',
+        //     'description' => 'jvjsj jdfjj dkfjd kdfj kfjk
+        //     svsvjk dfnvjjsv fdosvov oiofjvisojvsj oisvjoiv
+        //     skjvjsv'
         // ]);
+        // Listing::create([
+        //     'title' => 'Junior Developer',
+        //     'tags' => 'laravel javascript',
+        //     'company' => 'Acme Corp',
+        //     'location' => 'Boston, MA',
+        //     'email' => 'email1@mail.com',
+        //     'website' => 'https:/www.asne.com',
+        //     'description' => 'jvjsj jdfjj dkfjd kdfj kfjk
+        //     svsvjk dfnvjjsv fdosvov oiofjvisojvsj oisvjoiv
+        //     skjvjsv'
+
+        // ]);
+
+
     }
 }
